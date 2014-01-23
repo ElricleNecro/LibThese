f1195184cf8500b8a2519c597748503185faf7b0
ElricleNecro <guiplum@gmail.com>
2014-01-23 18:55:18 +0100

    Now test the presence of necessary packages. Import setuptools to use the findpackages function.

--- setup.py
+++ setup.py
@@ -7,35 +7,28 @@
 import os, sys, stat
 
 try:
-	import King
+	import InitialCond
 except ImportError:
-	print("It seems that the python Binding for the king librairies is not installed on your system.\nYou should install it as it is a necessary dependancy!")
+	print("You need the InitialCond package for some import part of the module.")
 	sys.exit(-1)
 
 try:
-	import commands
-except:
-	import subprocess as commands
+	import numpy
+except ImportError:
+	print("Numpy is a needed dependancy.")
+	sys.exit(-1)
 
+try:
+	import matplotlib
+except ImportError:
+	print("You really need matplotlib")
+	sys.exit(-1)
+
+#from setuptools import find_packages
+import setuptools as st
 from distutils.core import setup
 
-#--------------------------------------------------------------------------------------------------------------
-# For adding support of pkg-config:
-#--------------------------------------------------------------------------------------------------------------
-def scandir(dir, files=[]):
-	for file in os.listdir(dir):
-		path = os.path.join(dir, file)
-		if os.path.isfile(path) and path.endswith(".pyx"):
-			files.append(path.replace(os.path.sep, ".")[:-4])
-		elif os.path.isdir(path):
-			scandir(path, files)
-	return files
-
-#--------------------------------------------------------------------------------------------------------------
-# Packages names:
-#--------------------------------------------------------------------------------------------------------------
-from setuptools import find_packages
-packages = find_packages() # [ 'LibThese' ]
+packages = st.find_packages()
 
 #--------------------------------------------------------------------------------------------------------------
 # Call the setup function:
