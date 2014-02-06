e9726fb05f08b8d8757bdaeb89162f1f8854960e
ElricleNecro <guiplum@gmail.com>
2014-02-06 22:19:54 +0100

    Rename the old MapPlot class to PSPlot. Add a new command and class to do projection on a plan.

--- scripts/animation.py
+++ scripts/animation.py
@@ -4,6 +4,7 @@
 import os
 import argparse                   as ap
 import logging			  as log
+import numpy			  as np
 
 from LibThese      import Carte   as c
 from LibThese.Plot import Animate as an
@@ -37,12 +38,12 @@ class FromHDF5(object):
 
 		return frame
 
-class MapPlot(FromHDF5):
+class PSPlot(FromHDF5):
 	def __init__(self, args):
 		self._cb = None
 		self._J = args.J
 		self._j_bin = args.j_bin
-		super(MapPlot, self).__init__(args)
+		super(PSPlot, self).__init__(args)
 
 	@property
 	def J(self):
@@ -130,6 +131,78 @@ class LogDensity(FromHDF5):
 
 		return frame
 
+class MapPlot(FromHDF5):
+	def __init__(self, args):
+		self._cb = None
+		self._Plan = args.Plan
+		self._select = args.RSelect
+		self._nb_bin = args.nb_bin
+		self._spherical_selection = args.spherical_selection
+		self._resize_not_done = True
+		super(MapPlot, self).__init__(args)
+
+	@property
+	def Plan(self):
+		return self._Plan
+	@Plan.setter
+	def Plan(self, val):
+		self._Plan = val
+
+	@property
+	def Radius(self):
+		return self._select
+	@Radius.setter
+	def Radius(self, val):
+		self._select = val
+
+	def __call__(self, frame, ax, *o, **kwo):
+		if self._resize_not_done:
+			ax.figure.set_size_inches( np.array([1920, 1080]) / ax.figure.get_dpi() )
+			self._resize_not_done = False
+
+		p = None
+		try:
+			p = -self.File.get_time(os.path.basename(frame), "x", "y", "z")
+		except KeyError as e:
+			log.fatal("Unable to find Key: '" + frame + "'")
+			self.File.close()
+			raise e
+
+		map = c.Map(
+				frame,
+				nbbin=self._nb_bin,
+				RSelect=self._select,
+				#move_pos=p,
+				to_center=self._spherical_selection,
+		)
+
+		_, _, cb = map.Plot(
+				map,
+				self.Plan,
+				fig=ax.figure,
+				ax=ax,
+		)
+		if self._cb is not None:
+			self._cb.update_normal(cb)
+		else:
+			self._cb = ax.figure.colorbar(cb)
+
+		ind = map._tlist[self.Plan]
+		ax.plot(p[ind[0]], p[ind[1]], "r+", linewidth=10, markersize=12, markeredgewidth=13)
+		ax.text(
+			0.8,
+			0.95,
+			"Time: %g"%(
+				self._file.get_time(os.path.basename(frame), "time"),
+			),
+			transform=ax.transAxes,
+			verticalalignment='center',
+			horizontalalignment='left',
+			color="white",
+		)
+
+		return frame
+
 def set_common_args(parser):
 	parser.add_argument(
 		"Files",
@@ -184,7 +257,7 @@ def set_common_args(parser):
 def create_sub_PSMap(sub):
 	parser = sub.add_parser('psplot', help="Plot the phase space of multiple files.")
 	parser.set_defaults(
-		func=MapPlot,
+		func=PSPlot,
 		xscale="log",
 		yscale="linear",
 		xlim=(1e-2, 20.),
@@ -209,6 +282,47 @@ def create_sub_PSMap(sub):
 		default=None,
 	)
 
+def create_sub_Map(sub):
+	parser = sub.add_parser('projection', help="Plot a gadget file on a plan.")
+	parser.set_defaults(
+		func=MapPlot,
+		xscale="linear",
+		yscale="linear",
+		xlim=(-20., 20.),
+		ylim=(-20, 20),
+		xlabel=r"$r$",
+		ylabel=r"$\vec{r}.\vec{v}/r$",
+		title="Projection",
+	)
+	set_common_args(parser)
+
+	parser.add_argument(
+		"--plan",
+		dest="Plan",
+		type=str,
+		help="Plan to use.",
+		choices=[ a for a in c.Map.GetPermutation(["x", "y", "z"]).keys() ],
+		default="xy",
+	)
+	parser.add_argument(
+		"--nb-bin",
+		type=int,
+		help="Number of bins of angular momentum.",
+		default=300,
+	)
+	parser.add_argument(
+		"--selection-radius",
+		type=float,
+		help="Radius into which it will select particles. All particles outside this radius will not be considered.",
+		default=None,
+		dest="RSelect",
+	)
+	parser.add_argument(
+		"--spherical-selection",
+		action='store_true',
+		help="Are we using the 'Spherical selection' version of the verification code?",
+	)
+
 def create_sub_LogDensity(sub):
 	parser = sub.add_parser('density-log', help="Plot the density (with bin size constant in log) of multiple files.")
 	parser.set_defaults(
@@ -228,6 +342,7 @@ def get_args():
 	sub = parser.add_subparsers(help="What do we want to plot?")
 	create_sub_PSMap(sub)
 	create_sub_LogDensity(sub)
+	create_sub_Map(sub)
 
 	return parser.parse_args()
 
