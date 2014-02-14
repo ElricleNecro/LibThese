eafff8de490dd95edeaa70e1f46d84b21f10169a
ElricleNecro <guiplum@gmail.com>
2014-02-14 19:15:09 +0100

    Add a basic plug-ins system to the script.

--- scripts/animationv2.py
+++ scripts/animationv2.py
@@ -1,12 +1,32 @@
 #! /usr/bin/env python3
 # -*- coding:Utf8 -*-
 
+import os
+import sys
+import logging as log
+import importlib
+
 import matplotlib as ml
 ml.use('agg')
 
 import LibThese.Plot.Animation as lpa
 from LibThese.Plot import Animate as an
 
+PREFIX      = os.path.dirname(__file__)
+PLUGINS_DIR = os.path.abspath(
+			os.path.join(
+				PREFIX,
+				"../share/LibThese"
+			)
+		)
+
+sys.path.insert(0, PLUGINS_DIR)
+
+try:
+	importlib.import_module("animation-plugins")
+except ImportError as e:
+	log.fatal("Import failed. sys.path is: {0}".format(sys.path))
+	raise e
 
 if __name__ == '__main__':
 	args = lpa.get_args()
