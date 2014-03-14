85aba17d96210b4f2559e59c0b83d996a7d85664
ElricleNecro <guiplum@gmail.com>
2014-03-14 18:28:12 +0100

    Change python to python3 as the module is python3 only.

--- install.py
+++ install.py
@@ -4,11 +4,11 @@
 import sys, os
 import subprocess as s
 
-if s.call(["python", "setup.py"] + sys.argv[1:]) != 0:
+if s.call(["python3", "setup.py"] + sys.argv[1:]) != 0:
 	raise RuntimeError()
 
 if "--user" in sys.argv:
 	install_dir = os.path.join(os.path.expanduser("~"), ".local/")
-	s.call(["python", "setup.py", "install_data", "-d", install_dir])
+	s.call(["python3", "setup.py", "install_data", "-d", install_dir])
 else:
-	s.call(["python", "setup.py", "install_data"])
+	s.call(["python3", "setup.py", "install_data"])
